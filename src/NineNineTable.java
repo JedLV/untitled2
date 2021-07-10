8b8023e6a3816ec04daf89e830e9b91297472cc6
编程 <1660873558@qq.com>
2021-07-10 10:13:43 +0800

    九九乘法表

--- src/NineNineTable.java
+++ src/NineNineTable.java
@@ -10,6 +10,11 @@
 
 public class NineNineTable {
     public static void main(String[] args) {
-        System.out.println();
+        for (int i = 1; i <= 9; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.println(i + "*" + j + "=" + (i * j) + "");
+            }
+            System.out.println();
+        }
     }
 }
