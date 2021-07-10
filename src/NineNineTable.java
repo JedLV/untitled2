f858254511677167f763a06f47832740c958eb7d
编程 <1660873558@qq.com>
2021-07-10 10:08:46 +0800

    嵌套循环

---
+++ src/NineNineTable.java
@@ -0,0 +1,15 @@
+/*
+九九乘法表
+
+1 * 1 = 1
+2 * 1 = 2  2 * 2 = 4
+。。。
+9 * 1 = 9 。。。 9 * 9 = 81
+ */
+
+
+public class NineNineTable {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
